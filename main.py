d96fa52dad2ff0f4df6233eec8267001c102dec3
EggPudding <ksj970531@gmail.com>
2021-11-21 23:04:13 +0900

    Implement test function for http request

---
+++ main.py
@@ -0,0 +1,30 @@
+def inference(request):
+    import base64
+
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    print(request)
+
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+        
+    print(name)
+
+    return 'hello, world'
+
+if __name__ == '__main__':
+    pass
