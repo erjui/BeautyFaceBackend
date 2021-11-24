bacc57087bf6a2323dec3baf1de1b748a8b62a62
EggPudding <ksj970531@gmail.com>
2021-11-24 20:28:49 +0900

    Fix server to properly transfer image data with numpy as intermidiate step

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def inference(request):
     import PIL.Image
     import numpy as np
     from io import BytesIO
+    import json
+    import cv2
 
     """HTTP Cloud Function.
     Args:
@@ -13,7 +15,21 @@ def inference(request):
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    print(request)
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows POST requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+        
+
+    print('request', request)
 
     request_json = request.get_json(silent=True)
     request_args = request.args
@@ -25,17 +41,34 @@ def inference(request):
     else:
         name = 'World'
 
+    print('name', name)
+    print('json', request_json)
+
     cat = request_json['cat']
-    cat = BytesIO(base64.b64decode(cat))
-    img = PIL.Image.open(cat)
-    img = np.array(img)
+    print('bf decoding', cat)
+    cat = base64.b64decode(cat)
+    # cat = BytesIO(cat)
+    # img = PIL.Image.open(cat)
+    # img = np.array(img)
+    img = np.frombuffer(cat, dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
 
-    print(img.shape)
-    print(name)
+    print('img shape', img.shape)
 
-    img_base64 = base64.b64encode(img)
+    # img_base64 = base64.b64encode(img)
+    _, img_base64 = cv2.imencode('.jpg', img)
+    img_base64 = img_base64.tobytes()
+    img_base64 = base64.b64encode(img_base64)
 
-    return img_base64
+    print('af encoding', img_base64)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    # return img_base64, 
+    return (json.dumps({"cat": img_base64.decode('utf8')}), 200, headers)
 
 if __name__ == '__main__':
     pass
