0bf064413ae46bea1761201bb64a5d08d09fe9ea
EggPudding <ksj970531@gmail.com>
2021-11-25 16:56:42 +0900

    Improve model output visualization

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+color_dict = {
+    0: (0, 0, 0),
+    1: (128, 64, 128),
+    2: (244, 35, 232),
+    3: (70, 70, 70),
+    4: (102, 102, 156),
+    5: (190, 153, 153),
+    6: (153, 153, 153),
+    7: (250, 170, 30),
+    8: (220, 220, 0),
+    9: (107, 142, 35),
+    10: (152, 251, 152),
+    11: (70, 130, 180),
+    12: (220, 20, 60),
+    13: (255, 0, 0),
+    14: (0, 0, 142),
+    15: (0, 0, 70),
+    16: (0, 60, 100),
+    17: (0, 80, 100),
+    18: (0, 0, 230),
+    19: (119, 11, 32),
+}
+
+def labelVisualize(num_class, color_dict, img):
+    import numpy as np
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out
+
+
 def inference(request):
     import cv2
     import json
@@ -45,11 +77,7 @@ def inference(request):
     out = out.detach().cpu().numpy()
     out = np.uint8(out)
     out = cv2.resize(out, (shape[1], shape[0]), interpolation=cv2.INTER_NEAREST)
-    out = out * 10
-
-    # TODO: add label colorization
-
-    print(out.shape)
+    out = labelVisualize(19, color_dict, out)
 
     _, img_base64 = cv2.imencode('.jpg', out)
     img_base64 = img_base64.tobytes()
