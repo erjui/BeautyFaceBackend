38450e5ce7adec248e0236eaac9b35151eb79e26
EggPudding <ksj970531@gmail.com>
2021-11-25 16:48:13 +0900

    Use default memory size 2048MB

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 gcloud functions deploy inference \
     --region=us-central1 \
     --runtime=python39 \
-    --memory=512MB \
+    --memory=2048MB \
     --trigger-http \
 	--allow-unauthenticated
