Move img processing function into main function & Fix uint8 overflow problem

main.py
@@ -1,13 +1,6 @@
 # atts = [1 'skin', 2 'l_brow', 3 'r_brow', 4 'l_eye', 5 'r_eye', 6 'eye_g', 7 'l_ear', 8 'r_ear', 9 'ear_r',
 #         10 'nose', 11 'mouth', 12 'u_lip', 13 'l_lip', 14 'neck', 15 'neck_l', 16 'cloth', 17 'hair', 18 'hat']
 
-def lib_color_change(img, segment):
-    # RGB order
-    mask = (segment == 12) | (segment == 13)
-    img[..., 0][mask] += 50
-
-    return img
-
 def inference(request):
     import cv2
     import json
@@ -17,6 +10,17 @@ def inference(request):
     from model import SegmentModel
     from torchvision.transforms import transforms
     from utils import label_visualize
+
+    def lib_color_change(img, segment):
+        # RGB order
+        img = np.int32(img)
+        
+        mask = (segment == 12) | (segment == 13)
+        img[..., 0][mask] += 50
+        img[img > 255] = 255
+        # img[..., 0][mask] = cv2.add(img[..., 0][mask], 100)
+
+        return np.uint8(img)
 
     # Set CORS headers for the preflight request
     if request.method == 'OPTIONS':